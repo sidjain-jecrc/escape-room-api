892eae1fdd64d171868c9121fb1531f11903f63e
Siddharth Jain <siddharthjain.jecrc@gmail.com>
2026-02-06 13:50:26 -0800

    fixing docker container issues

--- src/main/java/com/example/escaperoom/repo/SlotRepository.java
+++ src/main/java/com/example/escaperoom/repo/SlotRepository.java
@@ -1,7 +1,6 @@
 package com.example.escaperoom.repo;
 
 import com.example.escaperoom.model.Slot;
-import com.example.escaperoom.model.SlotStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
